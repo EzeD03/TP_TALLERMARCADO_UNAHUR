5b72e9df53ec66a68062d3a7dc38d5f8750f9c5f
Geraldine Gutierrez <geraldine.gut13rrez@gmail.com>
2026-07-01 13:17:08 -0300

    fix: corrijo ruta del archivo main.js y carpetas para hacer funcionar el modo oscuro

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-// buscamos el boton en el HTML a traves de su ID
+// buscamos el botón en el HTML a través de su ID
 const boton = document.getElementById("btn-modo");
 
-// boton espera el click
+//  botón espera el clic
 boton.addEventListener("click", () => {
     
-    //hacen clic y le agregamos (o quitamos) la clase oscuro a todo el body
+    // agregamos (o quitamos) la clase "oscuro" a todo el body
     document.body.classList.toggle("oscuro");
     
-    // cambio de texto del botón para verse mejor
+    // cambiamos el texto del botón
     if (document.body.classList.contains("oscuro")) {
         boton.textContent = "☀️ Modo claro";
     } else {
